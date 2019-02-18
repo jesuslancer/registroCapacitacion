5e8c41254bf78228a27609a19c1421fc7e53b47d
Jesus Lancer <jesusalfredo.rivera@gmail.com>
2019-02-18 18:29:32 -0400

    Se elimino la carpeta bootstrap de public, aun no funciona.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
         <!-- Fonts -->
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/Controller/registroCaptacionCtrl.js') }}"></script>         
         <script src="{{ asset('css/app.css') }}"></script>
+        <script src="{{ asset('js/Controller/registroCaptacionCtrl.js') }}"></script>         
     </head>
     <!-- Styles -->
         <style>
