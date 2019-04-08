6e8f1ea3abb0963cbac70ffff06385af3ae9384c
Jesus Lancer <jesusalfredo.rivera@gmail.com>
2019-04-08 16:26:39 -0400

    subiendo luego de hacer npm run dev en windows

--- public/js/app.js
+++ public/js/app.js
@@ -58123,8 +58123,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/registroCaptacion/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/registroCaptacion/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\RegistroCaptacion\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\RegistroCaptacion\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
